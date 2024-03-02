3d3afeaaf1d7370ef33ccda1c843abfadb7fbf1b
Rebecavitoria45 <rebecanascimentovit@gmail.com>
2024-03-02 15:29:13 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Menus/MenuMostrarArtistas.cs
@@ -0,0 +1,27 @@
+﻿using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Menus
+{
+    internal class MenuMostrarArtistas : Menu
+    {
+        public override void Executar(Dictionary<string, Artista> musicasRegistradas)
+        {
+            base.Executar(musicasRegistradas);
+            ExibirTituloDaOpcao("Exibindo todos os artistas registradas na nossa aplicação");
+
+            foreach (string artista in musicasRegistradas.Keys)
+            {
+                Console.WriteLine($"Artista: {artista}");
+            }
+
+            Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
